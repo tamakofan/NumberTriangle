e78b9c17a1602ed294797aa1b1fa86b950b6c0f0
tamakofan <tamako.fan@mail.utoronto.ca>
2025-10-05 01:34:06 -0400

    task complete

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,7 @@
+import com.sun.jdi.IntegerValue;
+
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -88,8 +91,22 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+
+        int rtn = -1;
+
+        if (path.isEmpty()) {
+            rtn = getRoot();
+            return getRoot();
+            }
+
+        String next = path.substring(1);
+        if (path.charAt(0) == 'l') {
+            rtn = left.retrieve(next);
+        }
+        else if (path.charAt(0) == 'r') {
+            rtn = right.retrieve(next);
+        }
+        return rtn;
     }
 
     /** Read in the NumberTriangle structure from a file.
@@ -109,23 +126,54 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
+        ArrayList<NumberTriangle> past = new ArrayList<NumberTriangle>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
-        NumberTriangle top = null;
-
         String line = br.readLine();
+        int root_num = Integer.valueOf(line);
+
+        NumberTriangle top = new NumberTriangle(root_num);
+        past.add(top);
+
+        line = br.readLine();
         while (line != null) {
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            ArrayList<NumberTriangle> current = new ArrayList<NumberTriangle>();
+            String[] string_numbers;
+            string_numbers = line.split(" ");
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-            // TODO process the line
+            for (int i = 0; i < string_numbers.length; i++) {
+                 numbers.add(Integer.valueOf(string_numbers[i]));
+            }
+            // ok great now we have an ArrayList of the numbers in that line as ints supposedly
+
+            // remove when done; this line is included so running starter code prints the contents of the file
+            // System.out.println(line);
+
+            for (int i = 0; i < numbers.size(); i++) {
+                int this_num = (int) numbers.get(i);
+                NumberTriangle this_numtri = new NumberTriangle(this_num);
+                // we have our number triangle with the root
+
+                if (i == 0){
+                    past.get(0).setLeft(this_numtri);
+                } else if (i == (numbers.size() - 1)) {
+                    past.get(past.size() - 1).setRight(this_numtri);
+                }
+                else {
+                    past.get(i).setLeft(this_numtri); // left of your index
+                    past.get(i-1).setRight(this_numtri); // right of your index - 1
+                }
+
+                current.add(this_numtri); // add your numbertree to current
+            }
 
             //read the next line
             line = br.readLine();
+
+            past = current; // once it's through the whole line it'll replace the last row with the new row
         }
         br.close();
         return top;
